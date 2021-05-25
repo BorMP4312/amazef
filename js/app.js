f11f7fe857a756aed6c595fab576d5b0f9c1ea69
Maks <max.b.5@mail.ru>
2021-05-25 22:43:28 +0300

    test+tags

--- js/app.js
+++ js/app.js
@@ -23,7 +23,29 @@ var organizeByTags = function (toDoObjects) {
 var main = function (toDoObjects) {
     "use strict";
 
-	@@ -28,32 +50,8 @@ var main = function (toDoObjects) {
+    var toDos = toDoObjects.map(function (toDo) {
+        return toDo.description;
+    });
+    $(".tabs a span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
+            var $element = $(element),
+                $content;
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (var i = toDos.length; i > -1; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                };
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(2)")) {
+                console.log("Щелчок на вкладке Старые")
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
                 console.log("Щелчок на вкладке Теги");
 
@@ -32,7 +54,14 @@ var main = function (toDoObjects) {
                 organizedByTag.forEach(function (tag) {
                     var $tagName = $("<h4>").text(tag.name),
                         $content = $("<ul>");
-	@@ -68,16 +66,29 @@ var main = function (toDoObjects) {
+                    $tagName.addClass("tagsH4")
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
 
 
             } else if ($element.parent().is(":nth-child(4)")) {
