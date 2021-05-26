b314680b18fcaa747ad8931db81e95e50c67cb8b
Maks <max.b.5@mail.ru>
2021-05-26 10:20:45 +0300

    add mongo to server.js end of prakt 7

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express"),
 http = require("http"),
-app = express();
+mongoose = require("mongoose"),
+app = express(),
 toDos =[
 {
 "description" : "Купить продукты",
@@ -28,14 +29,43 @@ toDos =[
 }
 ]
 app.use(express.static("C:/Users/maxb5/Projects/Amazeriffic/client"));
+
+mongoose.connect('mongodb://localhost/amazeriffic');
+//mongoose.connect('mongodb://127.0.0.1/amazeriffic');
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
 http.createServer(app).listen(3000);
 app.get("/todos.json",function(req,res){
-	res.json(toDos);
+	ToDo.find({}, function (err, toDos) {
+        res.json(toDos);
+    });
 });
 app.use(express.urlencoded({ extended: true }));
 app.post("/todos", function (req, res) {
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
-	res.json({"message":"Вы размещаетеся на сервере!"});
+    console.log(req.body);
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
+    newToDo.save(function (err, result) {
+        if (err !== null) {
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            // клиент ожидает, что будут возвращены все задачи,
+            // поэтому для сохранения совместимости сделаем дополнитель) в папку part7 . Нам понадобится файл package.json , гденый запрос
+            ToDo.find({}, function (err, result) {
+                if (err !== null) {
+                    // элемент не был сохранен
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
+        
+    });
 });
